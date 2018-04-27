3711914c0e9e673fe55af44744898d4d1560fa4c
Evan Burrell <burrelle@oregonstate.edu>
2018-04-27 15:21:56 -0700

    Unit test 1 and 2

---
+++ projects/burrelle/dominion/unittest1.c
@@ -0,0 +1,104 @@
+/**
+ * Unit Test one for IsGameOver()
+ * unittest1.c
+ */
+ 
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include "rngs.h"
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+
+#define CHECK "\u2713"
+#define FAIL "\u274C"
+
+int main()
+{
+	// Variable declarations
+	struct gameState state;
+	int kingdom[10] = {
+		adventurer, 
+		gardens, 
+		embargo, 
+		village, 
+		minion, 
+		mine, 
+		cutpurse,
+        sea_hag, 
+        tribute, 
+        smithy
+    };
+
+    int isOver;
+    int passedTests = 0;
+    int failedTests = 0;
+
+    printf("Testing isGameOver function: \n");
+
+    initializeGame(2, kingdom, 2048, &state);
+
+
+ 	/**
+ 	 * Unit test 1
+ 	 */
+    isOver = isGameOver(&state);
+    if (!isOver)
+    {
+    	printf(CHECK " ");
+    	passedTests++;
+    }
+    else
+    {
+    	printf(FAIL " ");
+    	failedTests++;
+    }
+    printf("The game should not be over to start\n");
+    assert(!isOver);
+
+	/**
+ 	 * Unit test 2
+ 	 */
+    state.supplyCount[province] = 0;
+  	isOver = isGameOver(&state);
+    if (isOver)
+    {
+    	printf(CHECK " ");
+    	passedTests++;
+    }
+    else
+    {
+    	printf(FAIL " ");
+    	failedTests++;
+    }
+    printf("Setting the supplyCount of province cards to zero ends game\n");
+    assert(isOver);
+
+    /**
+     * Unit test 3
+     */
+    state.supplyCount[village] = 0;
+    state.supplyCount[minion] = 0;
+    state.supplyCount[mine] = 0;
+    isOver = isGameOver(&state);
+    if (isOver)
+    {
+    	printf(CHECK " ");
+    	passedTests++;
+    }
+    else
+    {
+    	printf(FAIL " ");
+    	failedTests++;
+    }
+    printf("Setting the supplyCount of three kingdom cards to zero ends game\n");
+    assert(isOver);
+
+    /**
+     * Statisics reporting
+     */
+    printf("isGameOver function unit test results: ");
+    printf("Passed: %d. Failed: %d\n", passedTests, failedTests);
+    
+	return 0;
+}
---
+++ projects/burrelle/dominion/unittest2.c
@@ -0,0 +1,125 @@
+/**
+ * Unit Test one for shuffle()
+ * unittest2.c
+ */
+
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include "rngs.h"
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+
+#define CHECK "\u2713"
+#define FAIL "\u274C"
+
+enum boolean
+{
+  false = 0,
+  true = 1
+};
+
+typedef enum boolean Bool;
+
+int compare(const int *a, const int *b);
+
+Bool compareTwoDecks(struct gameState *G)
+{
+	int i;
+	for (i = 0; i < G->deckCount[0]; i++)
+	{
+		if (G->deck[1][i] != G->deck[0][i])
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+
+int main()
+{
+  struct gameState state;
+  int kingdom[10] = {
+      adventurer,
+      gardens,
+      embargo,
+      village,
+      minion,
+      mine,
+      cutpurse,
+      sea_hag,
+      tribute,
+      smithy};
+
+  int deck[10];
+  int passedTests, failedTests;
+
+  printf("Testing the shuffle function: \n");
+
+  initializeGame(2, kingdom, 2048, &state);
+
+  /**
+ 	 * Unit test 1
+ 	 */
+  // Shuffle player 1
+  shuffle(0, &state);
+  // Shuffle player 2
+  shuffle(1, &state);
+  if (state.deckCount[1] == 10)
+  {
+    printf(CHECK " ");
+    passedTests++;
+  }
+  else
+  {
+    printf(FAIL " ");
+    failedTests++;
+  }
+  printf("There should be the same number of cards in both decks after shuffle\n");
+  assert(state.deckCount[1] == 10);
+
+  /**
+ 	 * Unit test 2
+ 	 * The shuffled cards have different cards
+ 	 */
+  // Shuffle player 1
+  shuffle(0, &state);
+  // Shuffle player 2
+  shuffle(1, &state);
+  if (compareTwoDecks(&state) != true)
+  {
+    printf(CHECK " ");
+    passedTests++;
+  }
+  else
+  {
+    printf(FAIL " ");
+    failedTests++;
+  }
+  printf("The cards should be shuffled independently\n");
+  assert(compareTwoDecks(&state) != true);
+
+/**
+ * Unit test 3
+ * The shuffled cards have different cards
+ */
+  qsort ((void*)(state.deck[0]), state.deckCount[0], sizeof(int), compare);
+  qsort ((void*)(state.deck[1]), state.deckCount[1], sizeof(int), compare);
+  
+  if (compareTwoDecks(&state) != false)
+  {
+    printf(CHECK " ");
+    passedTests++;
+  }
+  else
+  {
+    printf(FAIL " ");
+    failedTests++;
+  }
+  printf("After sorting the cards should be the same\n");
+  assert(compareTwoDecks(&state) != false);
+  
+
+  return 0;
+}
